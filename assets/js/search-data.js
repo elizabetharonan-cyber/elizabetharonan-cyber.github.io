8b0530e643dbc842fe1c4824820e776f9500f9a5
elizabetharonan-cyber <elizabeth.a.ronan@gmail.com>
2026-03-15 18:22:56 +0000

    Deploying to gh-pages from @ elizabetharonan-cyber/elizabetharonan-cyber.github.io@f002bffb43a0f8a42830c96f10f85ccf0b9eadbf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -514,11 +514,25 @@ ninja.data = [{
           window.open("mailto:%65%6C%69%7A%61%62%65%74%68.%61.%72%6F%6E%61%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/lizronan", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-7400-5718", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=8YQwM9IAAAAJ", "_blank");
         },
       },{
         id: 'social-rss',
@@ -528,18 +542,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.ncbi.nlm.nih.gov/myncbi/1niJzd-Lz-oAF/bibliography/public/", "_blank");
         },
       },{
       id: 'light-theme',

58831e00ec2cb75e04a4090df3d4923485623afb
elizabetharonan-cyber <elizabeth.a.ronan@gmail.com>
2026-03-16 04:21:49 +0000

    Deploying to gh-pages from @ elizabetharonan-cyber/elizabetharonan-cyber.github.io@f6e8f0aa8d69401fb1cb7e37398ce0e8c3125e28 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-about-me",
-          title: "About Me",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/about/";
-          },
-        },{id: "dropdown-research-articles",
+  },{id: "dropdown-research-articles",
               title: "Research Articles",
               description: "",
               section: "Dropdown",
